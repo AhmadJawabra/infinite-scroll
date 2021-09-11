e3dd35cd0e7b114e76b86143e8a6531c94b7b754
Ahmad Jawabra <ahmadjawabra95@gmail.com>
2021-09-11 15:56:25 +0300

    infinite scroll with javascript

--- script.js
+++ script.js
@@ -50,5 +50,13 @@ async function getPhotos() {
         console.error(error)
     }
 }
+// Check to see if scrolling near bottom of page, Load More Photos
+window.addEventListener('scroll', () => {
+    if ( window.innerHeight + window.scrollY >= document.body.offsetHeight - 1000 ) {
+        console.log('get photos');
+        getPhotos();
+    }
+})
 
+// On Load
 getPhotos()
